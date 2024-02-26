fe575d7496eff3e4d2bdcbd361e3dedeb6cef7ed
Andrei Maksimov <maksimov.andrei@gmail.com>
2024-02-26 06:14:01 -0500

    Add aws_account_id local variable

--- aurora/locals.tf
+++ aurora/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  aws_account_id                  = data.aws_caller_identity.current.account_id
   cluster_identifier              = "${local.prefix}-cluster"
   cluster_parameter_group_family  = var.cluster_parameter_group_family
   cluster_parameter_group_name    = replace("${local.prefix}-group", "[^a-z0-9.-]", "-")
